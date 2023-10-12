14399eed21b6c600fe3d6abfc7ed321267c704d8
francisco guerra <frncscgvdesarrollos@gmail.com>
2023-10-12 14:05:44 -0300

    app desde 0

--- prueba/src/App.tsx
+++ prueba/src/App.tsx
@@ -2,12 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>PRUEBA</h1>
-      <p>Probando 1..2..3</p>
-      <p>rama prueba</p>
-      <p>Este es el cambio</p>
-      <p>Esto es de la Rama Prueba 2</p>
-      <p>lalalalalalalala</p>
+      <h1>Hello Vite!</h1>
     </>
   )
 }
