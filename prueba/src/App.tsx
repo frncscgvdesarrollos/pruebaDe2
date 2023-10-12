a5deccff66df4fccd7276ab488f0dea3e7e5e94f
francisco guerra <frncscgvdesarrollos@gmail.com>
2023-10-12 17:29:51 -0300

    app desde 0, ahora con tailwind

--- prueba/src/App.tsx
+++ prueba/src/App.tsx
@@ -2,7 +2,8 @@ function App() {
 
   return (
     <>
-      <h1>Hello Vite!</h1>
+      <h1 className="text-6xl font-bold underline py-4">Hello with Vite! </h1>
+      <h2 className="text-5xl font-extrabold underline py-4">A practice of:  React - Typescript - Taildwind</h2>
     </>
   )
 }
